64d63ad4294ee885b02761d34e85d7ad4c60c5fb
shuvo2712 <shuvo2712@gmail.com>
2025-08-29 04:28:44 +0600

    history log

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// heart counter
+// Heart Counter
 const hearts = document.getElementsByClassName("card-heart");
 for (const heart of hearts) {
   heart.addEventListener("click", function () {
@@ -10,15 +10,20 @@ for (const heart of hearts) {
   });
 }
 
-// call button
+// Call Button
 const callBtns = document.getElementsByClassName("call-Btn");
 for (const call of callBtns) {
   call.addEventListener("click", function () {
+    //   get values
     const callCard = call.closest(".card");
     const cardName = callCard.querySelector(".card-Name").innerText;
     const cardNumber = callCard.querySelector(".card-Number").innerText;
     const coinCount = document.getElementById("coin-Count");
     const currentCoin = parseInt(coinCount.innerText);
+    //   get date & time
+    const callTime = new Date().toLocaleTimeString();
+    const callDate = new Date().toLocaleDateString();
+
     // check coin
     if (currentCoin < 20) {
       alert("Not enough coins!");
@@ -28,19 +33,34 @@ for (const call of callBtns) {
     coinCount.innerText = currentCoin - 20;
     alert(`Calling ${cardName} : ${cardNumber}.......`);
 
-    //   date & time
-    const callTime = new Date().toLocaleString();
-
     // add to history log
     const historyLog = document.getElementById("history-Log");
     const newLog = document.createElement("li");
-    newLog.innerText = `Called ${cardName} : ${cardNumber} at ${callTime}`;
-    newLog.classList.add("bg-gray-200", "p-2", "rounded");
+
+    newLog.classList.add(
+      "flex",
+      "justify-between",
+      "items-center",
+      "bg-gray-200",
+      "p-2",
+      "rounded-xl"
+    );
+
+    newLog.innerHTML = `
+        <div>
+          <p>${cardName}</p>
+          <p>${cardNumber}</p>
+        </div>
+        <div>
+          <p>${callTime}</p>
+            <p>${callDate}</p>
+        </div>
+      `;
     historyLog.prepend(newLog);
   });
 }
 
-// clear history button
+// Clear History button
 const clearHistoryBtn = document.getElementById("clear-History");
 const historyLog = document.getElementById("history-Log");
 clearHistoryBtn.addEventListener("click", function () {
